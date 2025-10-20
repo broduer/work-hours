139992d7832591bab4555583ace8d11e09c30bee
msamgan <mohdsamgankhan@gmail.com>
2025-10-20 19:39:55 -0400

    refactor master layout for improved aesthetics and user experience

--- resources/js/layouts/master-layout.tsx
+++ resources/js/layouts/master-layout.tsx
@@ -50,19 +50,37 @@ export default function MasterLayout({ children, breadcrumbs = [] }: MasterLayou
 
     return (
         <NotificationsProvider>
-            <div className="flex min-h-screen bg-gradient-to-br from-slate-50 to-blue-50/30 dark:from-slate-900 dark:to-slate-800 print:bg-white">
-                <div className="print:hidden">
+            <div className="relative flex min-h-screen bg-gradient-to-br from-blue-100 via-white to-indigo-100/40 dark:from-gray-900 dark:via-gray-850 dark:to-indigo-950/30 print:bg-white">
+                {/* Decorative background elements */}
+                <div
+                    className="absolute top-0 left-0 h-96 w-96 rounded-br-[20rem] bg-blue-500/10 dark:bg-blue-600/10"
+                    aria-hidden="true"
+                ></div>
+                <div
+                    className="absolute bottom-0 right-0 h-96 w-96 rounded-tl-[20rem] bg-indigo-500/10 dark:bg-indigo-600/10"
+                    aria-hidden="true"
+                ></div>
+                <div
+                    className="absolute top-1/3 right-1/4 h-64 w-64 rounded-full bg-blue-500/10 dark:bg-blue-600/10"
+                    aria-hidden="true"
+                ></div>
+                <div
+                    className="absolute bottom-1/4 left-1/4 h-48 w-48 rounded-full bg-indigo-400/5 dark:bg-indigo-500/5"
+                    aria-hidden="true"
+                ></div>
+
+                <div className="print:hidden relative z-10">
                     <MasterSidebar collapsed={collapsed} />
                 </div>
 
                 <TimeTrackerProvider>
-                    <div className={`flex-1 transition-all duration-500 ${pageLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
+                    <div className={`flex-1 transition-all duration-500 relative z-10 ${pageLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
                         <MasterContent breadcrumbs={breadcrumbs} collapsed={collapsed} setCollapsed={setCollapsed}>
                             {children}
                         </MasterContent>
                     </div>
 
-                    <div className="print:hidden">
+                    <div className="print:hidden relative z-10">
                         <MasterRightSidebar collapsed={collapsed} />
                     </div>
                 </TimeTrackerProvider>
